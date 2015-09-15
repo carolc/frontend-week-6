bdb845c528977dfe1626e7d85d843e76a423b277
Hackbright Student <info@hackbrightacademy.com>
2015-09-14 21:30:10 -0700

    week 6

--- code-appetizer/script.js
+++ code-appetizer/script.js
@@ -1,5 +1,11 @@
-/*
 
-  Your Javascript here
+var button = $("#myButton");
+var myColors = ["red", "blue", "green"];
+function moveToFoo(){
+    var myInput =$("#myInput").val();
+    var myDiv=$("#foo");
+    myDiv.append(myInput);
+    myDiv.css("background",myColors[0]);
+}
 
-*/
+button.click (moveToFoo());
--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,6 +1,29 @@
 jQuery(function(){
+var $home = $('#home');
+var $articles = $('article');
+$articles.hide();
+$home.show();
 
-  // Your javascript code here
+$("a").click (
+function ()
+{
+
+$("a").css("background","#fafafa");
+$(this).css("background","yellow");
+
+
+var getLink = $(this).attr("href");
+$articles.hide(); 
+
+var $myLink = $(getLink);
+$myLink.slideDown();
+
+}   
+)
+
+
+
+
 
 });
 
